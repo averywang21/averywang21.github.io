b3b8952731391e0af6777760fe1b56697ae5d660
Avery Wang <40705717+averywang21@users.noreply.github.com>
2025-10-19 15:10:45 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Welcome to your script file!
+console.log("Website loaded successfully!");
+
+// You can add interactive features here in the future.
+// For example, an alert when someone clicks a button:
+//
+// const myButton = document.querySelector("#my-button");
+// myButton.addEventListener("click", () => {
+//   alert("Button was clicked!");
+// });
